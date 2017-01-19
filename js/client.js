f51351c6253589704f49370e2be0a201a54f9350
Shane91 <scoish@live.com>
2017-01-19 13:13:25 -0500

    Initial Load

---
+++ js/client.js
@@ -0,0 +1,101 @@
+var socket = io.connect('http://localhost:8082');
+var game = new Game('#board', socket);
+var passcode;
+var userId;
+var checkGameReady;
+
+socket.on('*', function(event,data){
+  console.log(event);
+  console.log(data);
+});
+
+socket.on('fullGame', function(){
+  $('#notification').addClass('alert alert-danger').text('That game is full');
+})
+
+socket.on('gameJoined', function(newUserId){
+  userId = newUserId;
+  $('#prompt').hide();
+  $('#game').show();
+  checkGameReady = setInterval(function(){
+    socket.emit('isGameReady',passcode);
+  },15);
+})
+
+socket.on('initBoard', function(squares,usernames,flipped){
+  clearInterval(checkGameReady);
+  game.initBoard(squares,usernames,flipped);
+});
+
+socket.on('opponentLeft', function(){
+  alert('Opponent has left the game');
+  window.location.reload();
+})
+
+socket.on('setTurn', function(user){
+  game.setTurn(user);
+});
+
+socket.on('notYourTurn', function(){
+  alert('Not your turn ya dummy!');
+})
+
+socket.on('notYourChecker', function(){
+  alert('Not your checker ya dummy!');
+})
+
+socket.on('potentialSquares', function(potentialSquares){
+  game.showPotentialMoves(potentialSquares);
+});
+
+socket.on('reloadSquares', function(checker,originSquare,targetSquare){
+  game.reloadSquares(checker,originSquare,targetSquare);
+});
+
+socket.on('updateScores', function(scores){
+  game.updateScores(scores);
+})
+
+socket.on('updateChecker', function(checker){
+  game.updateChecker(checker);
+});
+
+socket.on('gameOver', function(winningId){
+  if(winningId == userId){
+    $('#result').html('<h1>Winner!</h1>');
+  }else{
+    $('#result').html('<h1>Loser :(</h1>');
+  }
+  $('#results-container').show();
+})
+
+$(function(){
+  $('#join').click(function(e){
+    username = $('#username').val();
+    passcode = $('#passcode').val();
+    joinGame(username, passcode, socket);
+  });
+
+});
+
+$(window).on('beforeunload', function(){
+  socket.emit('leaveGame', passcode, userId);
+})
+
+function joinGame(username, passcode, socket){
+  if(username != ''){
+    socket.emit('joinGame', {username: username}, passcode);
+  }
+}
+
+function checkUsers(){
+  socket.emit('waitingForUsers', passcode);
+}
+
+function checkForMoves(squareId,onlyJumps){
+  socket.emit('checkForMoves',squareId,onlyJumps,passcode);
+}
+
+function submitMove(checker,square){
+  socket.emit('processMove',userId,checker.id,square.id,passcode);
+}
