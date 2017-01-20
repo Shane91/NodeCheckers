f06b097cad69c936424c35cdef9f20824fcdefa9
Shane91 <scoish@live.com>
2017-01-19 21:19:17 -0500

    Style updates

--- js/checkers.js
+++ js/checkers.js
@@ -29,7 +29,7 @@ Game.prototype = {
       var squareElement = $('<div class="square" style="height:'+squareDimension+'px;width:'+squareDimension+'px;"></div>');
       if(square.playable){squareElement.addClass('black');}
       if(square.checker){
-        var checkerElement = $('<div class="checker"></div>');
+        var checkerElement = $('<div class="checker"><div class="checker-piece"></div></div>');
         checkerElement.addClass(square.checker.colour);
         if(square.checker.userId == userId){checkerElement.addClass('owner')};
         checkerElement.appendTo(squareElement);
@@ -63,7 +63,7 @@ Game.prototype = {
 
 
     $.each(potentialSquares, function(index,value){
-      value.element.addClass('possible-move');
+      value.element.addClass('possible-move animated pulse infinite');
     })
   },
   reloadSquares: function(checkerId,originSquare,targetSquare){
@@ -74,8 +74,22 @@ Game.prototype = {
     originSquareObj = game.squares[(originSquare.x*GAME_DIMENSION)+originSquare.y];
     targetSquareObj = game.squares[(targetSquare.x*GAME_DIMENSION)+targetSquare.y];
 
-    targetSquareObj.element.append(checkerObj.element);
     checkerObj.element.removeClass('moving');
+
+    leftOffset = targetSquareObj.element.offset().left-originSquareObj.element.offset().left;
+    topOffset = targetSquareObj.element.offset().top-originSquareObj.element.offset().top;
+
+    if(this.$board.hasClass('flipped')){
+      leftOffset *= -1;
+      topOffset *= -1;
+    }
+
+    checkerObj.element.css('transform','translate('+leftOffset+'px, '+topOffset+'px)')
+    checkerObj.element.one('webkitTransitionEnd otransitionend oTransitionEnd msTransitionEnd transitionend', function(e) {
+      checkerObj.element.css('transform','');
+      targetSquareObj.element.append(checkerObj.element);
+    });
+
     moving = null;
 
     originSquareObj.checker = null;
@@ -101,7 +115,7 @@ Game.prototype = {
   },
   setTurn: function(user){
     this.isTurn = user.id == userId ? true : false;
-    turnLabel = this.isTurn ? 'Your Turn' : 'Opponent\'s Turn';
+    turnLabel = user.username+'\'s Turn';
     $('#display-turn').text(turnLabel);
     if(this.isTurn){
       $('#display-turn').addClass('animated');
@@ -148,7 +162,7 @@ $('body').on('click','.checker.owner', function(e){
 
     if(moving != null){
       moving.element.removeClass('moving');
-      $('.square.possible-move').removeClass('possible-move');
+      $('.square.possible-move').removeClass('possible-move animated');
 
       if(target == moving){
         moving = null;
@@ -177,368 +191,6 @@ $('body').on('click','.square.possible-move',function(e){
   })
 
   submitMove(moving,target);
-  $('.square.possible-move').removeClass('possible-move');
+  $('.square.possible-move').removeClass('possible-move animated');
   moving = null;
-  /*
-  rowMovement = Math.abs(moving.square.row-target.row);
-  if(rowMovement > 1){
-    startingRow = moving.square.row;
-    startingCol = moving.square.col;
-    endRow = target.row;
-    endCol = target.col;
-    jumpedChecker = $.findFirst(checkers, function(elm){
-      return elm.square.row == (startingRow+endRow)/2 && elm.square.col == (startingCol+endCol)/2;
-    });
-    jumpedChecker.element.remove();
-    jumpedChecker.active = false;
-    jumpedChecker.square.checker = null;
-
-    var teamScore = $('.score-count[data-team="'+moving.team+'"]');
-    var teamScoreVal = parseInt(teamScore.text());
-    teamScore.text(++teamScoreVal);
-  }
-
-  moving.element.appendTo(target.element).removeClass('moving');
-  $('.square.possible-move').removeClass('possible-move');
-  
-
-  moving.square.checker = null;
-  moving.square = target;
-  target.checker = moving;
-  moving = null;
-
-  if(checkForMoves(target,target.checker,true).length){
-    console.log('another one');
-    console.log(moving);
-    target.checker.element.trigger('click');
-  }else{
-    console.log('no more');
-    
-    setTurn();
-  }
-  */
 })
-
-/*
-function checkForMoves(originSquare,movingChecker,onlyJumps=false){
-  var potentialSquares = [];
-  var jumpSquares = [];
-  potentialSquares = $.grep(game.squares, function(square){
-    if(square.x == originSquare.x+movingChecker.defaultDirection && (square.y == originSquare.y-1 || square.y == originSquare.y+1) && square.playable){
-      if(onlyJumps){
-        if(square.checker != null){
-          if(square.checker.team != movingChecker.team){
-            var checkCol = square.y+(square.y-originSquare.y);
-            var checkRow = square.x+movingChecker.defaultDirection;
-
-            var checkSquare = $.findFirst(squares, function(elm){
-              return elm.x == checkRow && elm.y == checkCol;
-            });
-
-            if(checkSquare)
-              if(checkSquare.checker == null)
-                jumpSquares.push(checkSquare);
-          }
-        }
-      }else{
-        if(!square.checker){return true;}
-        else{
-          console.log(square.checker.userId);
-          console.log(movingChecker.userId);
-
-          if(square.checker.userId != movingChecker.userId){
-            var checkCol = square.y+(square.y-originSquare.y);
-            var checkRow = square.x+movingChecker.defaultDirection;
-
-            var checkSquare = $.findFirst(game.squares, function(elm){
-              return elm.x == checkRow && elm.y == checkCol;
-            });
-
-            if(checkSquare)
-              if(!checkSquare.checker)
-                jumpSquares.push(checkSquare);
-          }
-        }
-      }
-    }
-    return false;
-    
-  });
-
-  
-  if(jumpSquares.length){
-    $.each(jumpSquares,function(index,square){
-      var moreJumps = checkForMoves(square,movingChecker,true);
-      $.merge(potentialSquares,moreJumps);
-    });
-  }
-  $.merge(potentialSquares,jumpSquares);
-  return potentialSquares;
-}
-*/
-
-/*
-      $.extend({
-        findFirst: function(elems, validateCb){
-          var i;
-          for (i=0; i<elems.length;++i) {
-            if(validateCb(elems[i],i))
-              return elems[i];
-          }
-          return undefined;
-        }
-      });
-
-      var board = $('#board');
-      var squares = [];
-      var checkers = [];
-      var teamTurn;
-
-      function initBoard(dimension=8){
-        squares = [];
-        checkers = [];
-        teamTurn = null;
-        board.html('');
-
-        maxSquareHeight = board.height()/dimension; //the max dimension that will work within the board height
-        maxSquareWidth = board.width()/dimension; //the max dimension that will work within the board width
-
-        squareDimension = maxSquareHeight <= maxSquareWidth ? maxSquareHeight : maxSquareWidth;
-
-        for (var i = 0; i < dimension; i++) {
-          evenRow = i%2==0 ? true : false;
-          var square;
-
-          for (var n = 0; n < dimension; n++){
-            playable = evenRow ? (n%2==0 ? false : true) : (n%2==0 ? true : false);
-            square = $('<div class="square" style="height:'+squareDimension+'px;width:'+squareDimension+'px;"></div>')
-            square.appendTo(board);
-
-            if(n == 0 && square){square.css('clear','both');}
-
-            squares.push({
-              element: square,
-              playable: playable,
-              coords: {
-                top: square.offset().top,
-                left: square.offset().left,
-                bottom: square.offset().top+square.height(),
-                right: square.offset().left+square.width()
-              },
-              row: i,
-              col: n,
-              checker: null
-            })
-
-            if(playable){square.addClass('black');}else{square.addClass('white');}
-          }
-        }
-
-        setCheckers(dimension);
-        setTurn();
-      }
-
-      function setTurn(){
-        teamTurn = teamTurn == undefined || teamTurn == 2 ? 1 : 2;
-        $('#current-turn').text(teamTurn == 1 ? 'Red' : 'Blue');
-      }
-
-      function sizeSquares(dimension=8){
-        maxSquareHeight = board.height()/dimension; //the max dimension that will work within the board height
-        maxSquareWidth = board.width()/dimension; //the max dimension that will work within the board width
-
-        squareDimension = maxSquareHeight <= maxSquareWidth ? maxSquareHeight : maxSquareWidth;
-
-        $.each(squares,function(index,value){
-          value.element.height(squareDimension).width(squareDimension);
-        })
-      }
-
-      function setCheckers(dimension){
-        checkerRows = Math.ceil(dimension/3);
-        checkerNum = dimension*checkerRows;
-
-        var checker;
-
-        for (var i = 0; i < checkerNum; i++) {
-          if(squares[i].playable){
-            checker = $('<div class="checker red"></div>');
-            checker.appendTo(squares[i].element);
-            checkers.push({
-              element: checker,
-              square: squares[i],
-              defaultDirection: 1,
-              team: 1,
-              active: true,
-              king: false
-            });
-            squares[i].checker = checkers[checkers.length-1];
-          }
-        }
-
-        for (var i = squares.length-1; i >= squares.length-checkerNum; i--) {
-          if(squares[i].playable){
-            checker = $('<div class="checker blue"></div>');
-            checker.appendTo(squares[i].element);
-            checkers.push({
-              element: checker,
-              square: squares[i],
-              defaultDirection: -1,
-              team: 2,
-              active: true,
-              king: false
-            });
-            squares[i].checker = checkers[checkers.length-1];
-          }
-        } 
-      }
-
-      function checkForMoves(originSquare,movingChecker,onlyJumps=false){
-        var potentialSquares = [];
-        var jumpSquares = [];
-        potentialSquares = $.grep(squares, function(e){
-          if(e.row == originSquare.row+movingChecker.defaultDirection && (e.col == originSquare.col-1 || e.col == originSquare.col+1) && e.playable){
-            if(onlyJumps){
-              if(e.checker != null){
-                if(e.checker.team != movingChecker.team){
-                  var checkCol = e.col+(e.col-originSquare.col);
-                  var checkRow = e.row+movingChecker.defaultDirection;
-
-                  var checkSquare = $.findFirst(squares, function(elm){
-                    return elm.row == checkRow && elm.col == checkCol;
-                  });
-
-                  if(checkSquare)
-                    if(checkSquare.checker == null)
-                      jumpSquares.push(checkSquare);
-                }
-              }
-            }else{
-              if(e.checker == null){return true;}
-              else{
-                if(e.checker.team != movingChecker.team){
-                  var checkCol = e.col+(e.col-movingChecker.square.col);
-                  var checkRow = e.row+movingChecker.defaultDirection;
-
-                  var checkSquare = $.findFirst(squares, function(elm){
-                    return elm.row == checkRow && elm.col == checkCol;
-                  });
-
-                  if(checkSquare)
-                    if(checkSquare.checker == null)
-                      jumpSquares.push(checkSquare);
-                }
-              }
-            }
-          }
-          return false;
-        });
-        if(jumpSquares.length){
-          $.each(jumpSquares,function(index,square){
-            var moreJumps = checkForMoves(square,movingChecker,true);
-            $.merge(potentialSquares,moreJumps);
-          });
-        }
-        $.merge(potentialSquares,jumpSquares);
-        return potentialSquares;
-      }
-
-      var moving = null;
-      $('body').on('click','.checker', function(e){
-        targetElement = $(this);
-        target = $.findFirst(checkers, function(elm){
-          return elm.element.is(targetElement);
-        });
-
-        if(target.team != teamTurn){
-          alert('Not your turn ya dummy!');
-        }else{
-          var newMoving;
-
-          if(moving != null){
-            moving.element.removeClass('moving');
-            $('.square.possible-move').removeClass('possible-move');
-
-            if(target == moving){
-              moving = null;
-            }else{
-              newMoving = target;
-            }
-          }else{
-            newMoving = target;
-          }
-            
-          if(newMoving){
-            moving = newMoving;
-
-            moving.element.addClass('moving');
-
-            var jumpSquares = [];
-
-            var potentialSquares = checkForMoves(moving.square,moving);
-            //Find potential squares
-
-            if(jumpSquares.length > 0){
-              $.merge(potentialSquares,jumpSquares);
-            }
-
-            $.each(potentialSquares, function(index,value){
-              value.element.addClass('possible-move');
-            })
-          }
-        }
-      });
-
-      $('body').on('click','.square.possible-move',function(e){
-        targetSquare = $(this);
-        target = $.findFirst(squares, function(elm){
-          return elm.element.is(targetSquare);
-        })
-
-        rowMovement = Math.abs(moving.square.row-target.row);
-        if(rowMovement > 1){
-          startingRow = moving.square.row;
-          startingCol = moving.square.col;
-          endRow = target.row;
-          endCol = target.col;
-          jumpedChecker = $.findFirst(checkers, function(elm){
-            return elm.square.row == (startingRow+endRow)/2 && elm.square.col == (startingCol+endCol)/2;
-          });
-          jumpedChecker.element.remove();
-          jumpedChecker.active = false;
-          jumpedChecker.square.checker = null;
-
-          var teamScore = $('.score-count[data-team="'+moving.team+'"]');
-          var teamScoreVal = parseInt(teamScore.text());
-          teamScore.text(++teamScoreVal);
-        }
-
-        moving.element.appendTo(target.element).removeClass('moving');
-        $('.square.possible-move').removeClass('possible-move');
-        
-
-        moving.square.checker = null;
-        moving.square = target;
-        target.checker = moving;
-        moving = null;
-
-        if(checkForMoves(target,target.checker,true).length){
-          console.log('another one');
-          console.log(moving);
-          target.checker.element.trigger('click');
-        }else{
-          console.log('no more');
-          
-          setTurn();
-        }
-        
-      })
-
-      $(function(){
-        initBoard();
-      })
-
-      $(window).resize(function(){
-        sizeSquares();
-      })
-*/
